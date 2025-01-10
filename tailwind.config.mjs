bfb911f38585529adb6fb054bee8c743e5b6c6c5
Kevin Flores Pacheco <arnold_k2001@hotmail.com>
2025-01-10 00:01:55 -0500

    added screens size custom

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,14 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			screens: {
+				'xs': '360px',
+				'sm': '640px',
+				'md': '768px',
+				'lg': '1024px',
+				'xl': '1280px',
+				'2xl': '1536px',
+			},
 			fontFamily: {
 				sans: ['Arial', 'sans-serif'],
 				monts: ['"Montserrat"', 'sans-serif'],
