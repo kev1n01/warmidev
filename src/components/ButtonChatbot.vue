d24667b4491d051d2569b217f0750fc21d9d39af
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-05-21 11:23:26 -0500

    feat: add BuyMeACoffee component and related assets; update ButtonChatbot and ThemeToggle components

--- src/components/ButtonChatbot.vue
+++ src/components/ButtonChatbot.vue
@@ -18,7 +18,7 @@ const translateLabels = useTranslations(
 <template>
   <button
     :title="translateLabels('chat.buttonfloat.title')"
-    class="fixed bottom-32 right-4 bg-white  font-semibold  stroke-2 rounded-full p-3 py-5 sm:bottom-24 sm:right-8 group opacity-0 text-black text-sm transition-opacity duration-200 ease-in-out"
+    class="fixed bottom-32 right-4 bg-white  font-semibold  stroke-2 rounded-full p-3 sm:bottom-24 sm:right-8 group opacity-0 text-black text-sm transition-opacity duration-200 ease-in-out"
     :class="{
       'opacity-100': !isHidden,
       'opacity-0': isHidden,
